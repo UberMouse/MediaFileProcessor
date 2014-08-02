4a606e0b8aa7f6093740672724e661654a300077
UberMouse <ubermouse894@gmail.com>
2014-08-02 22:10:58 +1200

    Formatting, updated DirectorySearcher constructor to take Trait instead of concrete

--- src/main/scala/nz/ubermouse/anime/ds/DirectorySearcher.scala
+++ src/main/scala/nz/ubermouse/anime/ds/DirectorySearcher.scala
@@ -4,9 +4,9 @@ import java.io.File
 
 case class AnimeFile(name: String, season: Int, episode: Int, fso: FileSystemObject)
 
-class DirectorySearcher(fs:FileSystem = new FileSystem) {
+class DirectorySearcher(fs: TFileSystem = new FileSystem) {
 
-  def apply(directory:File, animeTitles:Iterable[String]):Iterable[AnimeFile] = {
+  def apply(directory: File, animeTitles: Iterable[String]): Iterable[AnimeFile] = {
     val files = fs.getFilesIn(directory)
     files.map(fso => (fso, new FileNameParser(fso.name, animeTitles)))
          .filter{case(_, parser) => parser.isAnime}
@@ -14,5 +14,5 @@ class DirectorySearcher(fs:FileSystem = new FileSystem) {
          .map{case(fso, pa) => AnimeFile(pa.series, pa.season, pa.episode, fso)}
   }
 
-  def apply(path:String, animeTitles:Iterable[String]):Iterable[AnimeFile] = apply(new File(path), animeTitles)
+  def apply(path: String, animeTitles: Iterable[String]): Iterable[AnimeFile] = apply(new File(path), animeTitles)
 }
